e9840ee672805ada0b89f11c904b314603399e6f
tmtz <metzias@t-online.de>
2021-04-10 17:36:21 +0200

    makes box rotate

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,23 @@
-import React, { useState } from "react"
+import React, { useState, useRef } from "react"
 
-import { Canvas } from "react-three-fiber"
+import { Canvas, useRender } from "react-three-fiber"
 import { useSpring, a } from "react-spring/three"
 import "./style.css"
 
 const FBOBox = () => {
+  const meshRef = useRef()
   const [hovered, setHovered] = useState(false)
   const [active, setActive] = useState(false)
   const props = useSpring({
     scale: active ? [1.5, 1.5, 1.5] : [1, 1, 1],
     color: hovered ? "hotpink" : "gray",
   })
-
+  useRender(() => {
+    meshRef.current.rotation.y += 0.01
+  })
   return (
     <a.mesh
+      ref={meshRef}
       onPointerOver={() => setHovered(true)}
       onPointerOut={() => setHovered(false)}
       onClick={() => setActive(pre => !pre)}
