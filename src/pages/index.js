413f32a21291eae8bde2c7b064daf7ee62d97222
tmtz <metzias@t-online.de>
2021-04-10 19:24:33 +0200

    finetune stuff at box

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useRef } from "react"
+import * as THREE from "three"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 import { Canvas, extend, useThree, useRender } from "react-three-fiber"
 import { useSpring, a } from "react-spring/three"
@@ -23,6 +24,13 @@ const Controls = () => {
   )
 }
 
+const Plane = () => (
+  <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.5, 0]} receiveShadow>
+    <planeBufferGeometry attach="geometry" args={[100, 100]} />
+    <meshPhysicalMaterial attach="material" color="white" />
+  </mesh>
+)
+
 const FBOBox = () => {
   const [hovered, setHovered] = useState(false)
   const [active, setActive] = useState(false)
@@ -36,9 +44,10 @@ const FBOBox = () => {
       onPointerOut={() => setHovered(false)}
       onClick={() => setActive(pre => !pre)}
       scale={props.scale}
+      castShadow
     >
       <ambientLight />
-      <spotLight />
+      <spotLight penumbra={1} position={[0, 5, 10]} castShadow />
       <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
       <a.meshPhysicalMaterial attach="material" color={props.color} />
     </a.mesh>
@@ -46,8 +55,16 @@ const FBOBox = () => {
 }
 
 export default () => (
-  <Canvas>
+  <Canvas
+    camera={{ position: [0, 0, 5] }}
+    onCreated={({ gl }) => {
+      gl.shadowMap.enable = true
+      gl.shadowMap.type = THREE.PCFSoftShadowMap
+    }}
+  >
+    <fog attach="fog" args={["white", 5, 15]} />
     <Controls />
     <FBOBox />
+    <Plane />
   </Canvas>
 )
