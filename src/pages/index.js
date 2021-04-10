a3369ee3e478c328c2efc20a6978a9c84eefdccf
tmtz <metzias@t-online.de>
2021-04-10 17:12:21 +0200

    start to animate FBO box

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,28 @@
-import React from "react"
+import React, { useState } from "react"
 
 import { Canvas } from "react-three-fiber"
+import { useSpring, a } from "react-spring/three"
 import "./style.css"
 
 const FBOBox = () => {
+  const [hovered, setHovered] = useState(false)
+  const [active, setActive] = useState(false)
+  const props = useSpring({
+    scale: active ? [1.5, 1.5, 1.5] : [1, 1, 1],
+    color: hovered ? "hotpink" : "gray",
+  })
+
   return (
-    <mesh>
+    <a.mesh
+      onPointerOver={() => setHovered(true)}
+      onPointerOut={() => setHovered(false)}
+      onClick={() => setActive(pre => !pre)}
+      scale={props.scale}
+      // scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+    >
       <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-      <meshBasicMaterial attach="material" color="red" />
-    </mesh>
+      <a.meshBasicMaterial attach="material" color={props.color} />
+    </a.mesh>
   )
 }
 
